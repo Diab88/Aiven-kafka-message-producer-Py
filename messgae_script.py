18bfbffaa4e146581010989a23a165f5d4d16d48
Adam Diab <76654485+Diab88@users.noreply.github.com>
2023-10-25 17:00:28 +0200

    adding python script
    adding python script

---
+++ messgae_script.py
@@ -0,0 +1,62 @@
+import json
+from confluent_kafka import SerializingProducer
+import uuid
+from datetime import datetime
+import random
+
+hostname = "kafka-diab-diab88-sa-assignment.a.aivencloud.com"
+port = "23411"
+
+# Function to serialize the data into a JSON string
+def json_serializer(msg, s_obj):
+    return json.dumps(msg).encode('ascii')
+
+conf = {
+    'bootstrap.servers': hostname + ":" + port,
+    'client.id': 'myclient',
+    'security.protocol': 'SSL',
+    'ssl.ca.location': '/Users/diab/Desktop/Aiven/py/ca.pem',
+    'ssl.certificate.location': '/Users/diab/Desktop/Aiven/py/service.cert',
+    'ssl.key.location': '/Users/diab/Desktop/Aiven/py/service.key',
+    'value.serializer': json_serializer,
+    'key.serializer': json_serializer
+}
+
+producer = SerializingProducer(conf)
+
+# Function to produce mock IoT sensor data
+def produce_mock_iot_event():
+    # Generating a random UUID for the key
+    event_id = str(uuid.uuid4())
+    
+    # Generating a random temperature between 20 and 30
+    temperature = random.uniform(20, 30)
+    
+    # Getting the current timestamp in ISO 8601 format
+    timestamp = datetime.now().isoformat()
+
+    # Randomly choose a location for the sensor
+    locations = ["Living Room", "Bedroom", "Kitchen", "Garage"]
+    location = random.choice(locations)
+
+    # Creating the key and value for the Kafka message
+    key = {
+        "id": event_id
+    }
+    value = {
+        "id": event_id,
+        "timestamp": timestamp,
+        "temperature": round(temperature, 2),
+        "location": location
+    }
+
+    # Producing the message to the `iot_sensor_data` topic
+    producer.produce(
+        "iot_sensor_data",
+        key=key,
+        value=value
+    )
+    producer.flush()
+
+# Produce a mock IoT event
+produce_mock_iot_event()
